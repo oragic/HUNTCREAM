5290369bb8bde1c046e084d1709ad261fbe5c77a
Darknus <raidouglas_alves@outlook.com>
2021-02-12 09:02:37 -0300

    HUNTCREAM INIT

---
+++ src/pages/landingpage.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function landing(){
+    return(
+        <div>
+         <div id="page-landing">
+
+
+
+  *<img src={} alt="Logo da plataforma Happy" />
+
+  <main>
+        <h1>LOREM 
+      IPSUM
+    MORE</h1>
+                <p>is simply dummy text of the printing and typesetting industry.</p>
+
+  </main>
+
+  <div className="location">
+    <strong>São Paulo</strong>
+    <span>São Paulo</span>
+  </div>
+
+
+</div>
+        </div>
+
+
+    );
+}
