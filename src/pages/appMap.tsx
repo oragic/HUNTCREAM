e18f0a4ed129f872d04ae02b9c7898750d81333f
Darknus <raidouglas_alves@outlook.com>
2021-02-23 18:08:43 -0300

    was added libs for operator's map

--- src/pages/appMap.tsx
+++ src/pages/appMap.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 import { FiArrowRight } from 'react-icons/fi';
 
-import mapmark from '../images/Mapmark.png'
+import mapmark from '../images/Mapmark.png';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 function appMap(){
     return(
@@ -23,6 +24,18 @@ function appMap(){
                     <span>Piauí</span>
             </footer>
       </aside>
+
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+            <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+        </Marker>
+        </MapContainer>
 
 
         </div>    
