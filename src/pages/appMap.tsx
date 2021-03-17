66485d2461d524dd1fd7ff5ef23567e0abea6a95
Darknus <raidouglas_alves@outlook.com>
2021-03-16 23:16:04 -0300

    was Added map-aplication

--- src/pages/appMap.tsx
+++ src/pages/appMap.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 
-import { FiArrowRight } from 'react-icons/fi';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import '../styles/appMap.css';
 
 import mapmark from '../images/Mapmark.png';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Link } from 'react-router-dom';
 
 function appMap(){
     return(
         <div className="appMap-content">
        
-       <FiArrowRight size={26} color="rgba(8, 0, 0, 0.6)" />
+       
 
-       <aside>
+       <aside  >
+
             <header>
+            <Link to="" className="return">
+        <FiArrowLeft size={26} color="rgba(8, 0, 0, 0.6)" />
+        </Link>
                     <img src={mapmark} alt="loga do mapa huntcream" />
 
-                    <h2>I FELL LIKE A SUMMER</h2>
+                    <h2>I FELL <br/> LIKE A <br/> SUMMER</h2>
                     <p>Many desktop publishing packages and web page</p>
             </header>
 
@@ -25,17 +32,27 @@ function appMap(){
             </footer>
       </aside>
 
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+            <MapContainer   className="leaflet-container" 
+            center={[-23.6821604, -46.8754915]}
+            zoom={10}
+            style={{ width: "100%", height: "100%", position: "absolute" }} 
+            >
+
         <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Marker position={[51.505, -0.09]}>
-            <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+            <Popup    closeButton={false}
+                minWidth={240}
+                maxWidth={240}
+                className="map-popup"
+            >
+                blablabal           
             </Popup>
         </Marker>
         </MapContainer>
+       
 
 
         </div>    
